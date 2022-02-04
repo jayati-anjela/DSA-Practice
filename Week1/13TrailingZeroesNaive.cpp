60552ddb75f2b43a78feaf6e29405337e5422123
jayati-anjela <jayati.minj01@gmail.com>
2022-02-05 02:17:41 +0530

    week 31 to 6th

---
+++ Week1/13TrailingZeroesNaive.cpp
@@ -0,0 +1,20 @@
+int countZero(int n)
+{
+    int fact = 1;
+    // calc factorial
+    for(int i=2; i<=n; i++)
+    {
+        fact = fact * i;
+    }
+
+    // this part counts the trailing zeroes
+    int res = 0;
+    while(fact%10==0)
+    {
+        res++;
+        fact = fact/10;
+    }
+    return res;
+}
+
+// causes overflow for higher values of n
