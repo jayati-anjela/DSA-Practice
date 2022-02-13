0045c3dd39a6ac70567ccd68b78096987305ea2c
jayati-anjela <jayati.minj01@gmail.com>
2022-02-13 23:07:44 +0530

    7Feb - 13Feb

---
+++ TRACK3/6_SparseOrNot.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+// C++ program to check if n is sparse or not
+#include<iostream>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to check if the number is sparse or not.
+    bool isSparse(int n)
+    {
+        // Your code
+        int x = 3;
+		 while(x <= n){
+		     int tmp = n & x;
+		     if((tmp & (tmp - 1)) > 0)  return false;
+		     x <<= 1;
+		 }
+		 return true;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int t,n;
+    
+    //testcases
+    cin>>t;
+    while(t--)
+    {
+        //initializing n
+        cin>>n;
+        
+        //printing 1 if isSparse() returns true
+        //else 0
+        Solution ob;
+        if(ob.isSparse(n)){
+            cout << "1" << endl;
+        }
+        else cout << "0" << endl;
+        
+    }
+    return 0;
+}
+  // } Driver Code Ends
