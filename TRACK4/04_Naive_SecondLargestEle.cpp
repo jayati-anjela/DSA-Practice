0aa987443f974636ba24f2202fa30c6389d17386
jayati-anjela <jayati.minj01@gmail.com>
2022-06-16 11:06:38 +0530

    update

---
+++ TRACK4/04_Naive_SecondLargestEle.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int getlargest(int a[],int n)
+{
+    int largest=0;
+    
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>a[largest])
+        {
+            largest=i;
+        }
+    }
+    return largest;
+}
+
+int secondlargest(int a[],int n)
+{
+    int largest=getlargest(a,n);
+    int res=-1;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]!=a[largest])
+        {
+            if(res==-1)
+            res=i;
+            else if(a[i]>a[res])
+            res=i;
+        }
+    }
+    return res;
+}
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int pos=secondlargest(a,n);
+    printf("%d ",a[pos]);
+    
+	return 0;
+    
+}
